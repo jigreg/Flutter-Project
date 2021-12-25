c76ae511a544b35ca7f2ba97c63946c6c714d390
Yu Seo Jun <jigreg@naver.com>
2021-12-25 19:30:02 +0900

    Container Study

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,32 @@ class MyApp extends StatelessWidget {
           actions: [Icon(Icons.search),Icon(Icons.menu),Icon(Icons.notifications)],
           title: Text('금호동3가'),
         ),
-        body: SizedBox(
+        body: Container(
+          height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image.asset('Picture.png',width : 150,),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('카메라 팝니다'),
+                    Text('금호동 3가'),
+                    Text('7000원'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4')
+                      ],
+                    ),
+                  ],
+                ),
+
+              )
+            ],
+          ),
           ),
           )
         );
