fd74688ef9f51931229070615371f196f0a6627e
Yu Seo Jun <jigreg@naver.com>
2021-12-27 21:10:17 +0900

    ListView Study

--- lib/main.dart
+++ lib/main.dart
@@ -11,39 +11,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: Scaffold( //상중하로 레이아웃 나누는 것
-        appBar: AppBar(
-          actions: [Icon(Icons.search),Icon(Icons.menu),Icon(Icons.notifications)],
-          title: Text('금호동3가'),
+      home: Scaffold( //상중하로 레이아웃 나누는 것녕
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello'),
+          ],
         ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('Picture.png',width : 150,),
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라 팝니다'),
-                    Text('금호동 3가'),
-                    Text('7000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ],
-                    ),
-                  ],
-                ),
-
-              )
-            ],
-          ),
-          ),
           )
-        );
+    );
   }
 }
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text('안'),
+    );
+  }
+}
+
